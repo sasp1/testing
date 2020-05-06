9184ed855d44e681fefc8b15c70b8dd1184c699b
Sebastian Arcos Specht <s164394@student.dtu.dk>
2020-05-06 13:28:07 +0200

    Master

--- first/hey/Main.java
+++ first/hey/Main.java
@@ -15,5 +15,7 @@ public class Main {
         System.out.println("feature1");
         System.out.println("feature1");
         System.out.println("feature1");
+
+        System.out.println("master");
     }
 }
