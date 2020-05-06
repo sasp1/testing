5f8eed4689d1de4be7631fea0695be57b6196e9a
Sebastian Arcos Specht <s164394@student.dtu.dk>
2020-05-06 12:48:45 +0200

    Hello World

--- first/hey/Main.java
+++ first/hey/Main.java
@@ -10,5 +10,7 @@ public class Main {
         System.out.println("hej");
         System.out.println("hej");
         System.out.println("hej");
+        System.out.println("hej");
+        System.out.println("hej");
     }
 }
